8cbe82cd1bb305fbfaa1765690ffb78c93e93213
tridsanu taychathunyachot <tridsanu.tay@gmail.com>
2022-03-19 13:24:07 +0700

    RTCS service generate

---
+++ RTCS/src/main/java/com/example/RTCS/RtcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.RTCS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtcsApplication.class, args);
+	}
+
+}
